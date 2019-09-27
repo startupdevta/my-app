3fb05e07a253ac661ff59a51a43e1d05a5a1616d
StartupDevta <startupdevta@gmail.com>
2019-09-27 22:27:26 +0530

    Updates

---
+++ precache-manifest.e3cdc56a8b7243565204533eb9b21f18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaff4e09727c6066f375a6942aab1ec1",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "0ccdd44356c9cf6fcc34",
+    "url": "/my-app/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "a6f656ca0f400f5d3c6e",
+    "url": "/my-app/static/css/main.73aa5cf3.chunk.css"
+  },
+  {
+    "revision": "0ccdd44356c9cf6fcc34",
+    "url": "/my-app/static/js/2.f0f9cdef.chunk.js"
+  },
+  {
+    "revision": "a6f656ca0f400f5d3c6e",
+    "url": "/my-app/static/js/main.bbfc7326.chunk.js"
+  },
+  {
+    "revision": "9c47355d93f1cffa1d27",
+    "url": "/my-app/static/js/runtime~main.eed5ce82.js"
+  }
+]);
